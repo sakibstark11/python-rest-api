a3a15626c10d84d9b1e8711f45ba722afcf0f3f6
sakibstark11 <16sakib@gmail.com>
2025-08-03 11:14:19 +0000

    remove local storage

--- frontend/src/services/auth.ts
+++ frontend/src/services/auth.ts
@@ -7,25 +7,14 @@ class TokenManager {
 
   setToken(token: string | null) {
     this.currentToken = token;
-    if (token) {
-      localStorage.setItem('accessToken', token);
-    } else {
-      localStorage.removeItem('accessToken');
-    }
   }
 
   getToken(): string | null {
-    if (!this.currentToken) {
-      this.currentToken = localStorage.getItem('accessToken');
-    }
     return this.currentToken;
   }
 
   removeToken() {
     this.currentToken = null;
-    localStorage.removeItem('accessToken');
-    localStorage.clear();
-    sessionStorage.clear();
   }
 }
 
